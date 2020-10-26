91ac686800971c866858f0ce90c151b4c87b8db5
raphael62 <raphael.maillard@gmail.com>
2020-10-26 12:54:34 +0100

    little update

--- assets/admin.js
+++ assets/admin.js
@@ -19,32 +19,29 @@ import './fontawesome';
 
 console.log('Hello Webpack Encore! Edit me in assets/app.js');
 
-$(document).ready(function () {
-    /*
-    Add navbar background color when scrolled
-    */
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > 800) {
-            $(".navbar").addClass("bg-dark");
-        } else {
-            $(".navbar").removeClass("bg-dark");
-        }
-    });
-    // If Mobile, add background color when toggler is clicked
-    $(".navbar-toggler").click(function () {
-        if (!$(".navbar-collapse").hasClass("show")) {
-            $(".navbar").addClass("bg-dark");
-        } else {
-            if ($(window).scrollTop() < 800) {
-                $(".navbar").removeClass("bg-dark");
-            } else {}
-        }
-    });
-});
+// $(document).ready(function () {
+//     /*
+//     Add navbar background color when scrolled
+//     */
+//     $(window).scroll(function () {
+//         if ($(window).scrollTop() > 800) {
+//             $(".navbar").addClass("bg-dark");
+//         } else {
+//             $(".navbar").removeClass("bg-dark");
+//         }
+//     });
+//     // If Mobile, add background color when toggler is clicked
+//     $(".navbar-toggler").click(function () {
+//         if (!$(".navbar-collapse").hasClass("show")) {
+//             $(".navbar").addClass("bg-dark");
+//         } else {
+//             if ($(window).scrollTop() < 800) {
+//                 $(".navbar").removeClass("bg-dark");
+//             } else {}
+//         }
+//     });
+// });
 
-$(document).ready(function () {
-    $('[data-toggle="popover"]').popover();
-});
 $(document).ready(function () {
     $('[data-toggle="popover"]').popover();
 });
