66dcd0ac953a9eb06543960b310a6bc0118241ad
Raphael-Maillard <raphael.maillard@gmail.com>
2021-01-04 16:27:56 +0100

    Add form contact in modal

--- src/Controller/TestNewController.php
+++ src/Controller/TestNewController.php
@@ -4,10 +4,12 @@ namespace App\Controller;
 
 use App\Entity\Project;
 use App\Entity\Technologie;
+use App\Form\ContactFromType;
 use App\Repository\CategoryRepository;
 use App\Repository\ProjectRepository;
 use App\Repository\TechnologieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TestNewController extends AbstractController
@@ -15,12 +17,19 @@ class TestNewController extends AbstractController
     /**
      * @Route("/", name="test_new")
      */
-    public function index(TechnologieRepository $technologieRepository, ProjectRepository $projectRepository)
+    public function index(TechnologieRepository $technologieRepository, ProjectRepository $projectRepository, Request $request)
     {
+        $form = $this->createForm(ContactFromType::class);
+
+        $contact = $form->handleRequest($request);
+
         return $this->render('test_new/index.html.twig', [
             'controller_name' => 'TestNewController',
             'Technologies' => $technologieRepository->findAllASC(),
             'Projects' => $projectRepository->findAll(),
+            'contact' =>$form->createView() 
+
+            
             ]
         );
     }
--- src/Form/ContactFromType.php
+++ src/Form/ContactFromType.php
@@ -4,14 +4,50 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ContactFromType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('field_name')
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+
+            ->add('email', EmailType::class, [
+                'label' => 'Votre email',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('message', TextareaType::class, [
+                    'label' => 'Votre message'
+            ])
+
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'Êtes-vous sûr d\'envoyer ce message',
+                    ]),
+                ],
+            ])
         ;
     }
 
